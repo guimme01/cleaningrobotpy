1071ce99cf34d85766bf29bbe699519f32cbb420
guimme01 <immediataguido@gmail.com>
2024-12-09 01:17:31 +0100

    [GREEN 11] US3

--- src/cleaning_robot.py
+++ src/cleaning_robot.py
@@ -99,6 +99,8 @@ class CleaningRobot:
                 self.heading = self.E
             elif self.heading == self.E:
                 self.heading = self.S
+            elif self.heading == self.S:
+                self.heading = self.W
         return self.robot_status()
 
     def obstacle_found(self) -> bool:
